f4cac6ba3d783f82c9e60e06a1246b88227a640b
alaintanguy <61603038+alaintanguy@users.noreply.github.com>
2025-09-15 23:45:31 -0700

    Update build.gradle.kts

--- deviceapp/build.gradle.kts
+++ deviceapp/build.gradle.kts
@@ -11,6 +11,13 @@ android {
     targetSdk = 34
     versionCode = 1
     versionName = "1.0"
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
   }
   buildTypes {
     release {
@@ -25,4 +32,6 @@ dependencies {
   implementation("com.google.android.material:material:1.12.0")
   implementation("com.google.android.gms:play-services-location:21.2.0")
   implementation("com.google.android.gms:play-services-wearable:18.1.0")
+implementation("com.google.android.gms:play-services-tasks:18.1.0")
+
 }
